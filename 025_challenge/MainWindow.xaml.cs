01e8845a6b0d46343fa434489e2ce4029934f2fb
MessyAttorney16 <70332474+MessyAttorney16@users.noreply.github.com>
2021-01-28 10:45:43 -0600

    commit of final

--- 025_challenge/MainWindow.xaml.cs
+++ 025_challenge/MainWindow.xaml.cs
@@ -22,31 +22,70 @@ namespace _025_challenge
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
-        
-        int itemNumber;
-        
-        
+        //starting variables and arrays
+        string[] items;
+        string[] prices;
+        int itemNumber = 0;
+        int length;
+        int num;
+        double total;
 
-        public void button1_Click(object sender, RoutedEventArgs e)
+        private void button1_Click(object sender, RoutedEventArgs e)
         {
-            //take number entered and make it have value and make array of same size as the number entered
-            itemNumber = int.Parse(itemNumberBox.Text);
-            string[] items = new string[itemNumber];
-
-            //Change Labels 
+            //Change Labels            
             enterItemsLabel.Content = "Enter Items Below";
             itemNumberLabel.Content = "1";
-            numberOfItemsLabel.Content = itemNumber;
+            numberOfItemsLabel.Content = 0;
+
+            //make variables 
+            itemNumber = int.Parse(itemNumberBox.Text);
+            length = itemNumber;
+            num = length;
+
+            //disable button and clear label
+            button1.IsEnabled = false;
+            button1.Content = "Number of Items in List:";
+            label1.Content = "";
+
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
+            //make size of arrays according to user input
+            items = new string[itemNumber];
+            prices = new string[itemNumber];
 
+            //loop for adding items as long as num > 0
+            if (num > 0)
+            {
+                //add items in text boxes to array
+                items[length - num] = itemBox.Text;
+                prices[length - num] = itemPriceBox.Text;
 
+                //change labels to display new info each time item is added
+                itemListLabel.Content += "\n  " + items[length - num] + " " + "$" + prices[length - num];
+                itemNumberLabel.Content = (length - num) +2;
+                numberOfItemsLabel.Content = (length - num) + 1;
 
-            itemListLabel.Content = 
+                //add total each time button is pressed and display
+                total += double.Parse(itemPriceBox.Text);
+                totalCostLabel.Content = "$" + total;
+
+                //subtract one from num each time till its at 0
+                num--;
+                
+                //once num == 0 disable add button and clear labels
+                if (num == 0) button2.IsEnabled = false;
+                if (num == 0) label2.Content = "";
+                if (num == 0) itemNumberLabel.Content = "";
+            }
+            
+            //clear textBoxes
+            itemBox.Text = "";
+            itemPriceBox.Text = "";
+
         }
     }
 }
